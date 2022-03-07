f73a3a9056e65bd063dc1e52b45ec7189b841496
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-07 13:22:29 +0100

    getting data from api

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { OpenWeatherMap } from './api/openWeatherMap';
+import { WeatherBit } from './api/weatherBit';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class WeatherService {
+  constructor(private configService: ConfigService) {}
+
   async getCurrentWeather(lat: number, lon: number, alternateSource: boolean) {
-    const response = {
-      temperature: 1,
-      pressure: 1,
-      humidity: 1,
-      source: 'its string',
-      alternateSource: alternateSource,
-    };
+    const weatherSource = alternateSource
+      ? new WeatherBit(this.configService, lat, lon)
+      : new OpenWeatherMap(this.configService, lat, lon);
+    const response = weatherSource.getCurrentWeatherData();
     return response;
   }
 }
