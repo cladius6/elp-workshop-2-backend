5e4053e2133bc89714a56786a7a6b38110838793
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-07 13:20:30 +0100

    configuration file updated

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,14 +1,20 @@
 export default () => ({
   weatherApi: {
     openWeatherMap: {
-      name: 'openweathermap.org',
+      name: 'OpenWeatherMap',
       host: 'https://api.openweathermap.org/',
+      baseURL: 'https://api.openweathermap.org/data/2.5/',
+      url: 'weather',
       key: process.env.OPENWEATHER_API_KEY,
+      unit: 'standard',
     },
     weatherBit: {
-      name: 'weatherbit.io',
+      name: 'WeatherBit',
       host: 'https://api.weatherbit.io/',
+      baseURL: 'https://api.weatherbit.io/v2.0/',
+      url: 'current',
       key: process.env.WEATHERBIT_API_KEY,
+      unit: 'metric',
     },
   },
 });
