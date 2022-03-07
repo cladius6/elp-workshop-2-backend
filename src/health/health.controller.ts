d58a40648e5315dbc99c6721043e39b037e7741e
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-07 08:10:46 +0100

    Healthcheck for apis added

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -11,4 +11,21 @@ export class HealthController {
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
   ) {}
+
+  @Get('/api')
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () =>
+        this.http.pingCheck(
+          'api.openweathermap.org',
+          `https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=${process.env.OPENWEATHER_API_KEY}`,
+        ),
+      () =>
+        this.http.pingCheck(
+          'api.weatherbit.io',
+          `https://api.weatherbit.io/v2.0/current?lat=0&lon=0&key=${process.env.WEATHERBIT_API_KEY}`,
+        ),
+    ]);
+  }
 }
