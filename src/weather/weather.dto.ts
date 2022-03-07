fc6e602c86bedd4b1ead3dd28d7bdde2077b42ee
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-07 23:38:15 +0100

    Addictional test and limitation for values

--- src/weather/weather.dto.ts
+++ src/weather/weather.dto.ts
@@ -1,11 +1,15 @@
-import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { ToBoolean } from 'src/decorators/toBoolean.decorator';
 
 export class CurrentWeatherDto {
   @IsNumber()
+  @Max(90.0)
+  @Min(-90.0)
   lat: number;
 
   @IsNumber()
+  @Max(180.0)
+  @Min(-180.0)
   lon: number;
 
   @ToBoolean()
