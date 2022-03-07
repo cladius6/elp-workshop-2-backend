5c613c528ed9ada1fe0c51c5ddbc935b1b156dca
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-07 09:46:10 +0100

    bugFixed: alternateSource boolean value was always true

--- src/weather/weather.dto.ts
+++ src/weather/weather.dto.ts
@@ -1,4 +1,5 @@
 import { IsBoolean, IsNumber } from 'class-validator';
+import { ToBoolean } from 'src/decorators/toBoolean.decorator';
 
 export class CurrentWeatherDto {
   @IsNumber()
@@ -7,6 +8,7 @@ export class CurrentWeatherDto {
   @IsNumber()
   lon: number;
 
+  @ToBoolean()
   @IsBoolean()
   alternateSource: boolean;
 }
